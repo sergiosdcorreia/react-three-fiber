cf5aff560a25f5152b609576f661b1443b0eba48
Sergio Correia <sergio.correia@travelchapter.com>
2020-04-17 15:36:28 +0100

    Added box to the scene and animated it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import { Canvas, useRender } from "react-three-fiber";
+import { useSpring, a } from "react-spring/three";
+
 import './App.css';
+
+const Box = () => {
+  const meshRef = useRef();
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? "hotpink" : "gray"
+  })
+
+  useRender(() => {
+    meshRef.current.rotation.y += 0.01
+  })
+
+  return (
+    <a.mesh
+      ref={meshRef}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Box />
+    </Canvas>
   );
 }
 
